505f1106ffd5716f8e8be1cd3b6c44814c6d30b9
red shark <58134602+tronj-np@users.noreply.github.com>
2024-05-24 12:11:46 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import api_token
 import logging
+from random import randint
 from telegram import Update
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 
@@ -11,11 +12,14 @@ logging.basicConfig(
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="I'm a bot, please talk to me!"
+        text="!автор - профиль автора\n!рандом 0 100 - случайное число в выбранном диапазоне\n/start - вывод этого сообщения"
     )
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    if update.message.text == "!автор":
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="@youalwayswereakiddersteve")
+    elif update.message.text.split(' ')[0] == "!рандом":
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=randint(int(update.message.text.split(' ')[1]), int(update.message.text.split(' ')[2])))
 
     
 if __name__ == '__main__':
